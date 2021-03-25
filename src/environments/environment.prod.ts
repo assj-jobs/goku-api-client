ff25393ddc7c163ae993399f0bc6b6ff8938a1f7
Antonio Salviano Soares Júnior <assj.jobs@gmail.com>
2021-03-24 22:38:55 -0300

    Correções de dependências para deploy na azure.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   GOKU_API: 'https://goku-api-server.herokuapp.com',
-  KEYCLOAK_API: 'https://54.165.133.5:8080'
+  KEYCLOAK_API: 'http://54.165.133.5:8080'
 };
