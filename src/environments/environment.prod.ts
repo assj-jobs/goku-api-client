dd88308141abd4a6f7f5cbc192e2b3c9ca5b0869
Antonio Salviano Soares Júnior <assj.jobs@gmail.com>
2021-03-25 23:17:41 -0300

    Ajuste para integração keycloak no heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   GOKU_API: 'https://goku-api-server.herokuapp.com',
-  KEYCLOAK_API: 'http://34.204.68.154:8080'
+  KEYCLOAK_API: 'https://goku-keycloak.herokuapp.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  GOKU_API: 'http://localhost:8080',
-  KEYCLOAK_API: 'http://localhost:8085'
+  //GOKU_API: 'http://localhost:8080',
+  //KEYCLOAK_API: 'http://localhost:8085'
   //PROD
-  //GOKU_API: 'https://goku-api-server.herokuapp.com',
-  //KEYCLOAK_API: 'http://34.204.68.154:8080'
+  GOKU_API: 'https://goku-api-server.herokuapp.com',
+  KEYCLOAK_API: 'https://goku-keycloak.herokuapp.com/'
 
 };
